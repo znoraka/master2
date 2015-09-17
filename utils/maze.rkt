a23b2658506428d4cef913f2e6cc3f65613d360a
Noé LE PHILIPPE <znoraka@gmail.com>
2015-09-17 13:41:17 +0200

    improved maze algorithm

--- utils/maze.rkt
+++ utils/maze.rkt
@@ -1,9 +1,9 @@
 #lang racket/gui
 
-(define cell-count-width 1200)
-(define cell-count-height 700)
-(define cell-width 1)
-(define cell-height 1)
+(define cell-count-width 300)
+(define cell-count-height 150)
+(define cell-width 4)
+(define cell-height 4)
 
 (struct wall (i j right? up?))
 
@@ -52,7 +52,7 @@
 (define (draw-grid dc)
   (for* ([i (in-range cell-count-width)]
          [j (in-range cell-count-height)])
-    (draw-cell (* i cell-width) (* j cell-height) (at cells i j) dc)))
+    (draw-cell (* i cell-width) (* j cell-height) (at cells i j))))
 
 (define (create-walls x y)
   (list (wall x y 1 0)
@@ -63,31 +63,45 @@
 (define (face-wall-empty? wall)
   (define (neighbour-count x y)
     (- 4
-       (at cells (max 0 (- x 1)) y)
-       (at cells (min (- cell-count-width 1) (+ x 1)) y)
-       (at cells x (max 0 (- y 1)))
-       (at cells x (min (- cell-count-height 1) (+ y 1)))))
+       (at cells (max 1 (- x 1)) y)
+       (at cells (min (- cell-count-width 2) (+ x 1)) y)
+       (at cells x (max 1 (- y 1)))
+       (at cells x (min (- cell-count-height 2) (+ y 1)))))
   
   (let ([x (+ (wall-i wall) (wall-right? wall))]
         [y (+ (wall-j wall) (wall-up? wall))])
-    (cond [(or (< x 0) (>= x cell-count-width)) #f]
-          [(or (< y 0) (>= y cell-count-height)) #f]
+    (cond [(or (< x 1) (>= x (- cell-count-width 1))) #f]
+          [(or (< y 1) (>= y (- cell-count-height 1))) #f]
           [(= 0 (at cells x y)) #f]
           [(> (neighbour-count x y) 1) #f]
           [else #t])))
 
-(define (move-loop walls x y)
-  ;; (let ([walls (shuffle (append walls (create-walls x y)))])
-  (let ([walls (append (shuffle (create-walls x y)) walls)])
+(define (move-loop walls x1 y1 x2 y2)
+  (draw-cell (* x1 cell-width) (* y1 cell-height) (at cells x1 y1))
+  (draw-cell (* x2 cell-width) (* y2 cell-height) (at cells x2 y2))
+
+  (let move-loop ([walls walls]
+                  [x x1]
+                  [y y1])
+  (let ([walls (if (> (random 100) 80)
+                   (append (shuffle (create-walls x y)) walls)
+                   (shuffle (append walls (create-walls x y))))])
     (let f ([wall (car walls)] [walls walls])
-      (cond [(empty? walls) (send game-canvas on-paint)]
-            [(face-wall-empty? wall)
+      (cond [(face-wall-empty? wall)
              (let ([x (+ (wall-i wall) (wall-right? wall))]
                    [y (+ (wall-j wall) (wall-up? wall))])
                (set-matrix! cells x y 0)
                (draw-cell (* x cell-width) (* y cell-height) (at cells x y))
                (move-loop (cdr walls) x y))]
-            [else (f (cadr walls) (cdr walls))]))))
+            [else (if (empty? (cdr walls))
+                      ;; (draw-grid (get-game-canvas-dc))
+                      (displayln "done")
+                      (f (cadr walls) (cdr walls)))])))))
+
+(define (draw-entrances x1 y1 x2 y2)
+  (send (get-game-canvas-dc) set-brush "red" 'opaque)
+  (draw-cell (* x1 cell-width) (* y1 cell-height) (at cells x1 y1))
+  (draw-cell (* x2 cell-width) (* y2 cell-height) (at cells x2 y2)))
 
 (define (start)
   (set! cells (let ([v (make-vector (get-width) 1)])
@@ -101,7 +115,12 @@
      (sleep 1)
      (send (get-game-canvas-dc) set-brush "black" 'opaque)
      (send (get-game-canvas-dc) draw-rectangle 0 0 (get-width) (get-height))
-     (send (get-game-canvas-dc) set-brush "white" 'opaque)
-     (move-loop '() 0 0))))
+     (let ([x1 (inexact->exact (* cell-count-width 0.25))]
+           [y1 0]
+           [x2 (inexact->exact (* cell-count-width 0.75))]
+           [y2 (- cell-count-height 1)])
+       (draw-entrances x1 y1 x2 y2)
+       (send (get-game-canvas-dc) set-brush "white" 'opaque)
+       (move-loop '() x1 (+ y1 1) x2 (- y2 1))))))
 
 (start)
