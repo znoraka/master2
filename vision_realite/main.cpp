19ef3380ecba2361c5fa5178fb06185c456f26cf
Znoraka <znoraka@gmail.com>
2015-11-26 10:15:13 +0100

    TP1 terminé

--- vision_realite/main.cpp
+++ vision_realite/main.cpp
@@ -5,9 +5,13 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 #include <string>
+#include "opencv2/features2d/features2d.hpp"
+#include <algorithm>
 
 using namespace cv;
 using namespace std;
+
+#define CONTROL_POINTS 18
 
 std::vector<Point2f> pointsG;
 std::vector<Point2f> pointsD;
@@ -31,7 +35,7 @@ void MyLine(Mat& image, Point start, Point end )
 
 void drawPoints(Mat& image, std::vector<Point2f> points) {
   for(auto i : points) {
-    circle(image, i, 5, Scalar(0, 0, 255), 3);
+    circle(image, i, 3, Scalar(0, 0, 255), 2);
   }
 }
 
@@ -41,14 +45,16 @@ void computeFundamentalMatrix() {
       std::cout << pointsG[i] << " - " << pointsD[i] << std::endl;
     }
 
-    fundamental = findFundamentalMat(Mat(pointsG), Mat(pointsD), CV_FM_8POINT);
+    fundamental = findFundamentalMat(Mat(pointsG), Mat(pointsD), FM_8POINT);
     std::cout << fundamental << std::endl;
   }
 }
 
 void CallBackFunc(int event, int x, int y, int flags, void* userdata, int whichImage)
 {
-  auto drawEpilines = [=](Mat F, Mat pts, Mat image, std::string windowName, int whichImage) {
+  auto drawEpilines = [=](Mat F, std::vector<Point2f> pts, Mat image, std::string windowName, int whichImage) {
+    F = findFundamentalMat(pointsD, pointsG, CV_FM_LMEDS);
+
     Point2f p;
     p.x = x; p.y = y;
 
@@ -58,16 +64,38 @@ void CallBackFunc(int event, int x, int y, int flags, void* userdata, int whichI
 	 
     computeCorrespondEpilines(pts, whichImage, F, out);
 
+    for(int i = 0; i < pointsG.size(); i++)
+      {
+	// Creating p1, p2, the two points. Please let me know if this can be done in fewer lines.
+	cv::Mat p1(3,1, CV_64FC1), p2(3,1, CV_64FC1);
+
+	p1.at<double>(0) = pointsG.at(i).x;
+	p1.at<double>(1) = pointsG.at(i).y;
+	p1.at<double>(2) = 1.0;
+
+	p2.at<double>(0) = pointsD.at(i).x;
+	p2.at<double>(1) = pointsD.at(i).y;
+	p2.at<double>(2) = 1.0;
+
+	// Print yFx for each pair of points. This should be 0 for all.
+	cout << "yFx = " << p1.t() * F * p2 << endl;
+      }
+
     for(auto i : out) {
       // std::cout << i << std::endl;
-      Point2d p1, p2;
-      if(whichImage == 1) {
-	p1.x = 0; p1.y = -i[2] / i[1];
-	p2.x = image.cols; p2.y =  -(i[2]+i[0]*image.cols)/i[1];
-      } else {
-	p1.x = image.cols; p1.y = -i[2] / i[1];
-	p2.x = 0; p2.y =  -(i[2]+i[0]*image.cols)/i[1];
-      }
+      Point p1, p2;
+      p1.x = 0; p1.y = -i[2] / i[1];
+      p2.x = image.cols; p2.y =  -(i[2]+i[0]*image.cols)/i[1];
+
+      // std::cout << Mat(p1).t() * F * Mat(p2) << std::endl;
+
+      // if(whichImage == 1) {
+      // 	p1.x = 0; p1.y = -i[2] / i[1];
+      // 	p2.x = image.cols; p2.y =  -(i[2]+i[0]*image.cols)/i[1];
+      // } else {
+      // 	p1.x = image.cols; p1.y = -i[2] / i[1];
+      // 	p2.x = 0; p2.y = -(i[2]+i[0]*image.cols)/i[1];
+      // }
 
       MyLine(image, p1, p2);
     }
@@ -77,9 +105,9 @@ void CallBackFunc(int event, int x, int y, int flags, void* userdata, int whichI
   
   if  ( event == EVENT_LBUTTONDOWN ) {
     if(whichImage == 1) {
-      drawEpilines(fundamental, Mat(pointsD), imageG, "WindowG", 2);
+      drawEpilines(fundamental, pointsD, imageG, "WindowG", 1);
     } else {
-      drawEpilines(fundamental.t(), Mat(pointsG), imageD, "WindowD", 1);
+      drawEpilines(fundamental, pointsG, imageD, "WindowD", 2);
     }
   }
 }
@@ -89,11 +117,11 @@ void CallBackFuncG(int event, int x, int y, int flags, void* userdata) {
   if ( event == EVENT_LBUTTONDOWN ) {
     Point2f p;
     p.x = x; p.y = y;
-    if(pointsG.size() < 8) {
+    if(pointsG.size() < CONTROL_POINTS) {
       pointsG.push_back(p);
       std::cout << pointsG.size() << std::endl;
     } else {
-      if(pointsD.size() == 8 && pointsD.size() == 8) {
+      if(pointsD.size() == CONTROL_POINTS && pointsD.size() == CONTROL_POINTS) {
 	computeFundamentalMatrix();
       }
       CallBackFunc(event, x, y, flags, userdata, 1);
@@ -107,12 +135,12 @@ void CallBackFuncD(int event, int x, int y, int flags, void* userdata) {
   if ( event == EVENT_LBUTTONDOWN ) {
     Point2f p;
     p.x = x; p.y = y;
-    if(pointsD.size() < 8) {
+    if(pointsD.size() < CONTROL_POINTS) {
       pointsD.push_back(p);
       std::cout << pointsD.size() << std::endl;
       
     } else {
-      if(pointsD.size() == 8 && pointsD.size() == 8) {
+      if(pointsD.size() == CONTROL_POINTS && pointsD.size() == CONTROL_POINTS) {
 	computeFundamentalMatrix();
       }
       CallBackFunc(event, x, y, flags, userdata, 2);
@@ -139,25 +167,14 @@ int main( int argc, char** argv )
         return -1;
     }
 
-    // pointsG.push_back(Point2f(137, 255));pointsD.push_back(Point2d(119, 254));
-    // pointsG.push_back(Point2f(158, 255));pointsD.push_back(Point2d(138, 255));
-    // pointsG.push_back(Point2f(153, 194));pointsD.push_back(Point2d(139, 194));
-    // pointsG.push_back(Point2f(123, 63));pointsD.push_back(Point2d(155, 61));
-    // pointsG.push_back(Point2f(69, 146));pointsD.push_back(Point2d(91, 147));
-    // pointsG.push_back(Point2f(236, 153));pointsD.push_back(Point2d(258, 152));
-    // pointsG.push_back(Point2f(20, 140));pointsD.push_back(Point2d(52, 138));
-    // pointsG.push_back(Point2f(142, 318));pointsD.push_back(Point2d(126, 317));
-
-
-    // pointsG.push_back(Point2f(118, 256)); pointsD.push_back(Point2f(137, 255));
-    // pointsG.push_back(Point2f(138, 256)); pointsD.push_back(Point2f(160, 256));
-    // pointsG.push_back(Point2f(86, 261)); pointsD.push_back(Point2f(99, 264));
-    // pointsG.push_back(Point2f(175, 270)); pointsD.push_back(Point2f(191, 271));
-    // pointsG.push_back(Point2f(128, 183)); pointsD.push_back(Point2f(132, 186));
-    // pointsG.push_back(Point2f(157, 182)); pointsD.push_back(Point2f(160, 182));
-    // pointsG.push_back(Point2f(88, 141)); pointsD.push_back(Point2f(69, 143));
-    // pointsG.push_back(Point2f(233, 146)); pointsD.push_back(Point2f(211, 148));
-    // computeFundamentalMatrix();
+    pointsG.push_back(Point2f(123, 59)); pointsD.push_back(Point2f(157, 55));
+    pointsG.push_back(Point2f(140, 253)); pointsD.push_back(Point2f(121, 255));
+    pointsG.push_back(Point2f(152, 191)); pointsD.push_back(Point2f(138, 191));
+    pointsG.push_back(Point2f(211, 148)); pointsD.push_back(Point2f(233, 145));
+    pointsG.push_back(Point2f(99, 260)); pointsD.push_back(Point2f(86, 259));
+    pointsG.push_back(Point2f(41, 149)); pointsD.push_back(Point2f(67, 149));
+    pointsG.push_back(Point2f(140, 117)); pointsD.push_back(Point2f(158, 113));
+    pointsG.push_back(Point2f(192, 272)); pointsD.push_back(Point2f(179, 268));
     
     namedWindow( "WindowG", WINDOW_AUTOSIZE );
     setMouseCallback("WindowG", CallBackFuncG, NULL);
@@ -169,12 +186,12 @@ int main( int argc, char** argv )
     // drawPoints(imageD, pointsD);
     imshow( "WindowD", imageD );
     
-    char k;
+    // char k;
 
-    k=cvWaitKey(0);
+    cvWaitKey(0);
 
-    if(k == 27)
-      return 0;
+    // if(k == 27)
+    //   return 0;
 }
 
 
